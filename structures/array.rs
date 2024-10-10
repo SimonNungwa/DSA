73bf20914d3838d3f6daedcb46fadde60ae9cb0c
SimonNungwa <simonnungwa99@gmail.com>
2024-10-10 15:55:09 +0100

    code refactor

--- structures/array.rs
+++ structures/array.rs
@@ -1,24 +1,3 @@
-let mut array: [i32; 3] = [0; 3];
-
-array[1] = 1;
-array[2] = 2;
-
-assert_eq!([1, 2], &array[1..]);
-
-
-for x in array {
-    print!("{x} ");
-}
-
-
-/*
-    // Rust Arrays
-    // data type
-    // ! data type
-    // default values
-*/
-
-
 // data type
 
 fn main() {
